979a4ad2ca9962baf55bbf623665149e3bbcf123
RoshanlalPrajapati <rooshanlal1200@gmail.com>
2022-12-07 15:25:56 +0530

    First commit

--- dist/landingpage.js
+++ dist/landingpage.js
@@ -1,3 +1,16 @@
+const text = document.querySelector(".sec-text")
+const textLoad = () =>{
+  setTimeout(() => {
+    text.textContent = "STUDY BETTER" 
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "IMAGINE BETTER" 
+  }, 4000);
+}
+textLoad()
+setInterval(textLoad, 8000)
+
+
 function changeMode(){
     var element = document.body
     element.classList.toggle('dark-mode')
